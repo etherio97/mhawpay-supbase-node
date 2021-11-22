9d121a0c80b1e575ecc6d0f12b47240d36d62aa8
Shin Maung Maung <shinmaungmaung@mytel.com.mm>
2021-11-22 16:20:20 +0630

    a

--- server.js
+++ server.js
@@ -5,8 +5,20 @@ import { PORT } from "./config";
 import { router } from "./routes";
 import { errorHandler } from "./src/error-handler";
 import "./src/admin";
+import axios from "axios";
+import { writeFileSync, existsSync } from "fs";
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  let serviceAccount = [process.cwd(), "serviceAccount.json"].join("/");
+  existsSync(serviceAccount) ||
+    axios
+      .get(process.env.FIREBASE_SERVICE_ACCOUNT)
+      .then(({ data }) =>
+        writeFileSync(serviceAccount, JSON.stringify(data), "utf-8")
+      );
+}
 
 app.use(helmet());
 
